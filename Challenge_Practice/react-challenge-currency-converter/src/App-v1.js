69dc653ea646bf8afd0b2fac2059723117591346
shallowaria <shallowaria@gmail.com>
2025-06-18 17:52:37 +0800

    practice of Currency Converter

--- Challenge_Practice/react-challenge-currency-converter/src/App-v1.js
+++ Challenge_Practice/react-challenge-currency-converter/src/App-v1.js
@@ -1,13 +1,13 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
 import { useEffect, useState } from "react";
 
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 export default function App() {
   const [amount, setAmount] = useState(1);
-  const [fromCur, setFromCur] = useState("USD");
-  const [toCur, setToCur] = useState("EUR");
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
   const [converted, setConverted] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
   useEffect(
     function () {
       async function convert() {
@@ -15,12 +15,13 @@ export default function App() {
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
         );
+
         const data = await res.json();
         setConverted(data.rates[toCur]);
         setIsLoading(false);
       }
+
       if (fromCur === toCur) return setConverted(amount);
-      if (amount <= 0) return setAmount(1);
       convert();
     },
     [amount, fromCur, toCur]
@@ -39,20 +40,20 @@ export default function App() {
         onChange={(e) => setFromCur(e.target.value)}
         disabled={isLoading}
       >
-        <option>USD</option>
-        <option>EUR</option>
-        <option>CAD</option>
-        <option>INR</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
       <select
         value={toCur}
         onChange={(e) => setToCur(e.target.value)}
         disabled={isLoading}
       >
-        <option>USD</option>
-        <option>EUR</option>
-        <option>CAD</option>
-        <option>INR</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
       <p>
         {converted} {toCur}
