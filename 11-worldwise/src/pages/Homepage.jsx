7d0c6df3f0ad686eb7100d216d3b504fd7dbd5d9
shallowaria <shallowaria@gmail.com>
2025-08-11 17:16:07 +0800

    Route&Link

---
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Worldwise</h1>
+
+      <Link to="/Pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Homepage;
