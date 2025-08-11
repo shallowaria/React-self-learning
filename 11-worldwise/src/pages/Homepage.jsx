385794c3dc1866a993f3c67de0482da79d55429e
shallowaria <shallowaria@gmail.com>
2025-08-11 19:30:53 +0800

    Route

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>Worldwise</h1>
 
       <Link to="/Pricing">Pricing</Link>
