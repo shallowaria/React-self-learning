56d05bca3f685b782bdc82f2ad4209beddd1ccfe
shallowaria <shallowaria@gmail.com>
2025-08-14 22:39:19 +0800

    Building the Pages

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
