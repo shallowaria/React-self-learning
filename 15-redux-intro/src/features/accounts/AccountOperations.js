a36068d19142a1dc85926efe04ed9c025d5498f3
shallowaria <shallowaria@gmail.com>
2025-09-11 22:04:43 +0800

    thunk 异步更新Redux Bank

--- 15-redux-intro/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/src/features/accounts/AccountOperations.js
@@ -14,13 +14,15 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
 
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -58,9 +60,12 @@ function AccountOperations() {
             <option value="USD">US Dollar</option>
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
+            <option value="CNY">China RMB</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
