1b64c099473b47ec6b3f2696dfe8bd018b51ef4b
shallowaria <shallowaria@gmail.com>
2025-09-15 23:10:57 +0800

    flex in Tailwind

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Aria</div>;
+  return <div className="hidden text-sm font-semibold md:block">Aria</div>;
 }
 
 export default Username;
