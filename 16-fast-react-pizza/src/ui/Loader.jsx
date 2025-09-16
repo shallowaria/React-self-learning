caa309daa5404ae78de521f53c46aadf91cca7b5
shallowaria <shallowaria@gmail.com>
2025-09-16 16:06:01 +0800

    Styling Menu and Cart

--- 16-fast-react-pizza/src/ui/Loader.jsx
+++ 16-fast-react-pizza/src/ui/Loader.jsx
@@ -2,7 +2,7 @@ function Loader() {
   return (
     //宽度 = 父元素(html（viewport）)宽度 - left - right = 100%，高度 = 父元素高度 - top - bottom = 100%。
     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
-      <div className="loader"></div>;
+      <div className="loader"></div>
     </div>
   );
 }
