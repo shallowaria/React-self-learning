6e9cfc1d3d5208e039b8d665de7940da739e9efe
shallowaria <shallowaria@gmail.com>
2025-09-20 15:37:13 +0800

    create a backend with Supabase and connect react

---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://fondoxhhuajjadmmafuj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvbmRveGhodWFqamFkbW1hZnVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDkyNjAsImV4cCI6MjA3MzkyNTI2MH0.ScN-Gh2_7sKkUbQ51X-qELZbgmACg8ZaCDPtlzgvtBk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
