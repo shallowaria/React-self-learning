438a8f7cb4d1026b3a45109880ae8c5f5fa442eb
shallowaria <shallowaria@gmail.com>
2025-09-23 15:35:13 +0800

    Set Booking layout

--- 17-the-wild-oasis/src/ui/Table.jsx
+++ 17-the-wild-oasis/src/ui/Table.jsx
@@ -87,7 +87,7 @@ function Row({ children }) {
   );
 }
 function Body({ data, render }) {
-  if (!data.length) <Empty>No data to show at the moment</Empty>;
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
 
   return <StyledBody>{data.map(render)}</StyledBody>;
 }
