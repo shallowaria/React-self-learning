00fb5b1974194bc3c2245dcfae051ee570adb3d7
shallowaria <shallowaria@gmail.com>
2025-09-26 19:37:44 +0800

    Final Done

--- 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("shallowaria@example.com");
-  const [password, setPassword] = useState("pass0316");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
