712d0fcec6246057dc18bb639921a5354705c8b3
shallowaria <shallowaria@gmail.com>
2025-09-29 20:24:53 +0800

    配置环境变量

--- 21-the-wild-oasis-website/app/page.js
+++ 21-the-wild-oasis-website/app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
         alt="Mountains and forests with two cabins"
       />
 
-      <div className="relative z-10 text-center">
-        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+      <div className="relative z-10 text-center translate-y-40">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal ">
           Welcome to paradise.
         </h1>
         <Link
