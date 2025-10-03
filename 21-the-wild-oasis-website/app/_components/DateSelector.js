0b22c87a464b04fbd76bfb7f1ef464398d35a567
shallowaria <shallowaria@gmail.com>
2025-10-03 15:58:56 +0800

    Data Fetching

--- 21-the-wild-oasis-website/app/_components/DateSelector.js
+++ 21-the-wild-oasis-website/app/_components/DateSelector.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -12,7 +14,7 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector() {
+function DateSelector({ settings, cabin, bookedDates }) {
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
@@ -21,8 +23,7 @@ function DateSelector() {
   const range = { from: null, to: null };
 
   // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  const { minBookingLength, maxBookingLength } = settings;
 
   return (
     <div className="flex flex-col justify-between">
@@ -35,7 +36,7 @@ function DateSelector() {
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
-        numberOfMonths={2}
+        // numberOfMonths={2}
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
--- 21-the-wild-oasis-website/app/_lib/data-service.js
+++ 21-the-wild-oasis-website/app/_lib/data-service.js
@@ -132,6 +132,8 @@ export async function getBookedDatesByCabinId(cabinId) {
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
 
+  // await new Promise((res) => setTimeout(res, 5000));
+
   if (error) {
     console.error(error);
     throw new Error("Settings could not be loaded");
